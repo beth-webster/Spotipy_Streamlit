2e5d2f326c238b5ad40e671951694ce4b049c6c2
Beth Lockwood <bethlockwood814@gmail.com>
2022-11-11 13:22:34 +0000

    modified:   spotipy_streamlit.py     removed unused import

--- spotipy_streamlit.py
+++ spotipy_streamlit.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 import spotipy
@@ -10,7 +9,6 @@ from helper_functions import id_from_url, get_artists, convert_df_csv, playlist_
 
 st.set_page_config(layout="wide")
 plt.style.use('seaborn-poster')
-sns.set_context('poster')
 
 
 header = st.container()
