a20537157651b4bd043cd5c5374a98953fd367c3
Beth Lockwood <bethlockwood814@gmail.com>
2023-03-13 15:03:02 +0000

    modified:   helper_functions.py         Added new helper functions for easier main code readability 	modified:   spotipy_streamlit.py         Used new helper functions throughout         Added "Download bundle data as CSV to load" button

--- helper_functions.py
+++ helper_functions.py
@@ -11,28 +11,108 @@ def id_from_url(url):
 
     except AttributeError:
         return 'invalid URL'
+    
+def id_from_url_for_file(url):
+    try:
+        url_regex = re.search(r"\['([^']+)'\]", url)
+
+        return url_regex.group(1)
+
+    except AttributeError:
+        return 'invalid URL'
 
 def get_artists(results):
     artist_list = []
     for artist in results['artists']:
-        for key in artist:
-                if key == 'name':
-                    artist_list.append((artist[key]))
+        try:
+            for key in artist:
+                try:
+                    if key == 'name':
+                        artist_list.append((artist[key]))
+                except:
+                    pass
+        except:
+            pass
     artists = '/'.join([str(artist) for artist in artist_list])
     return artists
 
 def get_copyright(results):
     copyright_list = []
     for copyright in results['copyrights']:
-        for key in copyright:
-                if key == 'text':
-                    copyright_list.append((copyright[key]))
+        try:
+            for key in copyright:
+                try:
+                    if key == 'text':
+                        copyright_list.append((copyright[key]))
+                except:
+                    pass
+        except:
+            pass
     copyrights = '/'.join([str(copyright) for copyright in copyright_list])
     return copyrights
 
+def ablumtracks_title_artist_num_id(track):
+    track_title = track.get('name')
+    track_number = track.get('track_number')
+    track_artist_names = get_artists(track) 
+    track_id = track.get('id')
+    return track_title, track_artist_names, track_number, track_id
+
+def tracks_isrc_artirst_title(track_info):
+    isrc = track_info['external_ids'].get('isrc')
+    artist_names = get_artists(track_info)
+    title = track_info.get('name')
+    return isrc, artist_names, title
+    
+def playlisttracks_isrc_title_artitst_date(track):
+    try:
+        title = track['track'].get('name')
+        try:
+            isrc = track['track']['external_ids'].get('isrc')
+            rel_date = track['track']['album'].get('release_date')
+            artist_names = get_artists(track['track'])
+        except:
+            artist_names = []
+    except:
+        pass
+    return isrc, title, artist_names, rel_date
+
+def album_upc_lab_date_title_artitst_copy(album_info):
+    bundle_upc = album_info['external_ids'].get('upc')
+    label = album_info.get('label')
+    bundle_rel_date = album_info.get('release_date')
+    bundle_title = album_info.get('name')
+    bundle_artists = get_artists(album_info) 
+    bundle_copyright = get_copyright(album_info)
+    num_tracks = album_info.get('total_tracks')
+    if int(num_tracks)>4:
+        price = 425
+        bundle_class = "A"
+    else:
+        price = 40
+        bundle_class = "S"
+    return bundle_upc, label, bundle_rel_date, bundle_title, bundle_artists, bundle_copyright, num_tracks, price, bundle_class
+
+def playlist_class_num_art_title_upc_lab_copy_url(track, sp):
+    try:
+        bundle_class = track['track']['album']['album_type']
+        bundle_id = track['track']['album']['id']
+        album_results_playlist = album_results(sp, bundle_id)
+        bundle_title = album_results_playlist.get('name')
+        bundle_artists = get_artists(album_results_playlist)
+        bundle_upc = album_results_playlist['external_ids'].get('upc')
+        num_tracks = album_results_playlist.get('total_tracks')
+        bundle_label = album_results_playlist.get('label')
+        bundle_copyright = get_copyright(album_results_playlist)
+        bundle_url = album_results_playlist['external_urls'].get('spotify')
+    except:
+        pass
+    return bundle_class, num_tracks, bundle_artists, bundle_title, bundle_upc, bundle_label, bundle_copyright, bundle_url
+
+
 @st.cache
 def convert_df_csv(df):
-    return df.to_csv()
+    return df.to_csv(index = False,header=False)
 
 @st.cache
 def album_results(sp,id):
--- spotipy_streamlit.py
+++ spotipy_streamlit.py
@@ -3,7 +3,7 @@ import pandas as pd
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from PIL import Image
-from helper_functions import id_from_url, get_artists, get_copyright, convert_df_csv, playlist_results, album_results, track_results
+from helper_functions import id_from_url, id_from_url_for_file, ablumtracks_title_artist_num_id, tracks_isrc_artirst_title, playlisttracks_isrc_title_artitst_date, album_upc_lab_date_title_artitst_copy, playlist_class_num_art_title_upc_lab_copy_url, convert_df_csv, playlist_results, album_results, track_results
 
 
 st.set_page_config(layout="wide")
@@ -36,44 +36,34 @@ if len(client_id) > 25 and len(client_secret) > 25 :
             st.write('Track Data')
             track_rows = []
             if url_type == 'album':
-                results = album_results(sp, id)
-                for track in results['tracks']['items']:
-                    track_title = track.get('name')
-                    track_number = track.get('track_number')
-                    track_artist_names = get_artists(track) 
-                    track_id = track.get('id')
-                    results_tracks = track_results(sp, track_id)
-                    isrc = results_tracks['external_ids'].get('isrc')
+                album_info = album_results(sp, id)
+                for track in album_info['tracks']['items']:
+                    try:
+                        track_title, track_artist_names, track_number, track_id = ablumtracks_title_artist_num_id(track)
+                        track_info = track_results(sp, track_id)
+                    except:
+                        pass
+                    for track_id in track_info:
+                        try:
+                            isrc = tracks_isrc_artirst_title(track_info)[0]
+                        except:
+                            pass
                     track_rows.append([isrc, track_title, track_artist_names, track_number])
                 df = pd.DataFrame(track_rows, columns = ["ISRC", "Title", "Artist","Track Number"])
 
             elif url_type == 'playlist':
                 results = sp.playlist(id, market = 'GB')
                 for track in results['tracks']['items']:
-                    if track['track'] is not None:
-                        title = track['track'].get('name')
-                        if track['track']['external_ids'] is not None:
-                            isrc = track['track']['external_ids'].get('isrc')
-                        else:
-                            isrc = '-'                    
-                        if track['track']['album'] is not None:
-                            rel_date = track['track']['album'].get('release_date')
-                        else:
-                            rel_date = '-'
-                        if track['track']['artists'] is not None:
-                            artist_names = get_artists(track['track'])
-                        else:
-                            artist_names = []
-                    else:
-                        title = '-'
+                    try:
+                        isrc, title, artist_names, rel_date = playlisttracks_isrc_title_artitst_date(track)
+                    except:
+                        pass
                     track_rows.append([isrc, title, artist_names, rel_date]) 
                 df = pd.DataFrame(track_rows, columns = ["ISRC", "Title", "Artist", "Release date"])       
             
             elif url_type == 'track':
-                results = track_results(sp,id)
-                isrc = results['external_ids'].get('isrc')
-                artist_names = get_artists(results)
-                title = results.get('name')
+                track_info = track_results(sp,id)
+                isrc, artist_names, title = tracks_isrc_artirst_title(track_info)
                 track_rows.append([isrc, title, artist_names])
                 df = pd.DataFrame(track_rows, columns = ["ISRC", "Title", "Artist"])
 
@@ -92,55 +82,48 @@ if len(client_id) > 25 and len(client_secret) > 25 :
             st.write('Bundle Data')
             bundle_rows = []
             if url_type == 'album':
-                results = album_results(sp,id)
-                bundle_upc = results['external_ids'].get('upc')
-                label = results.get('label')
-                bundle_rel_date = results.get('release_date')
-                bundle_title = results.get('name')
-                bundle_artists = get_artists(results) 
-                bundle_copyright = get_copyright(results)
+                album_info = album_results(sp,id)
+                bundle_upc, label, bundle_rel_date, bundle_title, bundle_artists, bundle_copyright, num_tracks, price, bundle_class = album_upc_lab_date_title_artitst_copy(album_info)
                 bundle_rows.append([bundle_upc, bundle_title, bundle_artists, bundle_rel_date, label, bundle_copyright])
                 df = pd.DataFrame(bundle_rows, columns = ["UPC", "Title", "Artist","Release Date", "Label", "Copyright"])
 
             elif url_type == 'playlist':
-                results = playlist_results(sp, id)
-                for track in results['tracks']['items']:
-                    if track['track']is not None:
-                        if track['track']['album'] is not None:
-                            bundle_class = track['track']['album']['album_type']
-                            bundle_id = track['track']['album']['id']
-                            album_results_playlist = album_results(sp, bundle_id)
-                            bundle_title = album_results_playlist.get('name')
-                            bundle_artists = get_artists(album_results_playlist)
-                            bundle_upc = album_results_playlist['external_ids'].get('upc')
-                            bundle_label = album_results_playlist.get('label')
-                            bundle_copyright = get_copyright(album_results_playlist)
-                        else:
-                            bundle_class = '-'
-                            bundle_artists = '-'
-                            bundle_title = '-'
-                            bundle_upc = '-'
-                            bundle_label = '-'
-                    else:
-                        bundle_class = '-'
-                        bundle_artists = '-'
-                        bundle_title = '-'
-                        bundle_upc = '-'
-                        bundle_label = '-'
-                        bundle_copyright = '-'
-                    bundle_rows.append([bundle_class, bundle_upc,  bundle_title, bundle_artists, bundle_label, bundle_copyright])
-                df = pd.DataFrame(bundle_rows, columns = ["Class", "UPC", "Title", "Artist", "Label", "Copyright"])
-            
+                urls_rows_for_files = []
+                playlist_info = playlist_results(sp, id)
+                for track in playlist_info['tracks']['items']:
+                    try:
+                        bundle_class, num_tracks, bundle_artists, bundle_title, bundle_upc, bundle_label, bundle_copyright, bundle_url = playlist_class_num_art_title_upc_lab_copy_url(track, sp)
+                        bundle_rows.append([bundle_class, num_tracks, bundle_upc,  bundle_title, bundle_artists, bundle_label, bundle_copyright, bundle_url])
+                        urls_rows_for_files.append([bundle_url])
+                    except:
+                        pass
+                df = pd.DataFrame(bundle_rows, columns = ["Class", "Number_of_Tracks","UPC", "Title", "Artist", "Label", "Copyright", "Bundle_URL"])
+                track_file_rows = []
+                tracklisting_file_rows = []
+                bundle_file_rows = []
+                for file_url in urls_rows_for_files:
+                    try:
+                        ext_str_file_url = id_from_url_for_file(str(file_url))
+                        file_url_type, file_url_id = id_from_url(ext_str_file_url)
+                        if file_url_type == 'album':
+                            files_album_info = album_results(sp,file_url_id)
+                            bundle_upc, label, bundle_rel_date, bundle_title, bundle_artists, bundle_copyright, num_tracks, price, bundle_class = album_upc_lab_date_title_artitst_copy(files_album_info)
+                        elif url_type == 'track':
+                            track_info = track_results(sp,id)
+                            bundle_id = track_info['album'].get('id')
+                            files_album_info = album_results(sp, bundle_id)
+                            bundle_upc, label, bundle_rel_date, bundle_title, bundle_artists, bundle_copyright, num_tracks, price, bundle_class = album_upc_lab_date_title_artitst_copy(album_info)
+                        bundle_file_rows.append([bundle_upc,"",bundle_title,bundle_artists,"",bundle_class,"A",label,label,label,bundle_copyright,price,"","",num_tracks])
+
+                    except:
+                        pass
+                bundle_file_df = pd.DataFrame(bundle_file_rows)
+                
             elif url_type == 'track':
-                results = track_results(sp,id)
-                bundle_id = results['album'].get('id')
-                results_bundle = album_results(sp, bundle_id)
-                bundle_upc = results_bundle['external_ids'].get('upc')
-                label = results_bundle.get('label')
-                bundle_rel_date = results_bundle.get('release_date')
-                bundle_title = results_bundle.get('name')
-                bundle_artists = get_artists(results_bundle)
-                bundle_copyright = get_copyright(results_bundle) 
+                track_info = track_results(sp,id)
+                bundle_id = track_info['album'].get('id')
+                album_info = album_results(sp, bundle_id)
+                bundle_upc, label, bundle_rel_date, bundle_title, bundle_artists, bundle_copyright, num_tracks, price, bundle_class = album_upc_lab_date_title_artitst_copy(album_info)
                 bundle_rows.append([bundle_upc, bundle_title, bundle_artists, bundle_rel_date, label, bundle_copyright])
                 df = pd.DataFrame(bundle_rows, columns = ["UPC", "Title", "Artist","Release Date", "Label", "Copyright"])
 
@@ -149,10 +132,17 @@ if len(client_id) > 25 and len(client_secret) > 25 :
 
             st.dataframe(df)
             csv = convert_df_csv(df)
-            st.download_button(label = "Download bundle data as CSV"
+            st.download_button(label = "Download above table as CSV"
                             , data = csv
                             , file_name = 'bundle_data.csv'
                             , mime = 'text/csv')
+            
+            bundle_csv = convert_df_csv(bundle_file_df)
+            st.download_button(label = "Download bundle data as CSV to load"
+                            , data = bundle_csv
+                            , file_name = 'bundle_data.csv'
+                            , mime = 'text/csv')
+            
     else:
         with track_data:
             st.write('Enter a spotify URL for an Album, Playlist or Track')
