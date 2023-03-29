3a5b2ea91000303c733a9ceb77e3f3de8117a3e6
Beth Lockwood <bethlockwood814@gmail.com>
2023-03-29 12:48:24 +0100

    modified:   helper_functions.py     newly cached functions

--- helper_functions.py
+++ helper_functions.py
@@ -10,30 +10,30 @@ def id_from_url(url):
     except AttributeError:
         return 'invalid URL'
     
-@st.cache
+@st.cache_data
 def convert_df_csv(df):
     return df.to_csv(index = False,header=False)
 
-@st.cache
 def album_results(sp,id):
     return sp.album(id, market = 'GB')
 
-@st.cache
 def playlist_results(sp,id):
     return sp.playlist(id, market = 'GB')
 
-@st.cache
 def track_results(sp,id):
     return sp.track(id, market = 'GB')
 
+@st.cache_data
 def album_upc(album_info):
     upc = album_info['external_ids'].get('upc')
     return upc
 
+@st.cache_data
 def album_label(album_info):
     label = album_info.get('label')
     return label
 
+@st.cache_data
 def album_copyright(album_info):
     copyright_list = []
     for copyright in album_info['copyrights']:
@@ -49,30 +49,37 @@ def album_copyright(album_info):
     copyrights = '/'.join([str(copyright) for copyright in copyright_list])
     return copyrights
 
+@st.cache_data
 def album_reldate(album_info):
     reldate = album_info.get('release_date')
     return reldate
 
+@st.cache_data
 def album_title(album_info):
     title = album_info.get('name')
     return title
 
+@st.cache_data
 def album_numtracks(album_info):
     numtracks = album_info.get('total_tracks')
     return int(numtracks)
 
+@st.cache_data
 def album_trackid(track):
     trackid = track.get('id')
     return trackid
 
+@st.cache_data
 def album_trackseq(track):
     trackseq = track.get('track_number')
     return trackseq
 
+@st.cache_data
 def track_isrc(track_info):
     isrc = track_info['external_ids'].get('isrc')
     return isrc
 
+@st.cache_data
 def artists(track):
     artist_list = []
     for artist in track['artists']:
@@ -88,14 +95,17 @@ def artists(track):
     artists = '/'.join([str(artist) for artist in artist_list])
     return artists
 
+@st.cache_data
 def track_title(track):
     title = track.get('name')
     return title
 
+@st.cache_data
 def playlist_bundleid(track):
     bundleid = track['track']['album']['id']
     return bundleid
 
+@st.cache_data
 def track_bundleid(track_info):
     bundleid = track_info['album'].get('id')
     return bundleid
