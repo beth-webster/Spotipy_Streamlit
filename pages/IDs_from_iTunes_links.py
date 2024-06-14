a8b4ce026d8dba28e7504ca80666f06d9218ea84
Beth Webster <bethlockwood814@gmail.com>
2024-06-14 15:15:27 +0100

    modified:   pages/IDs_from_iTunes_links.py     added section for flagging

--- pages/IDs_from_iTunes_links.py
+++ pages/IDs_from_iTunes_links.py
@@ -25,6 +25,7 @@ header = st.container()
 links_input = st.container()
 output_ids = st.container()
 compare_ids = st.container()
+flagging_isrcs = st.container()
 
 # Header Sction of page
 with header:
@@ -44,12 +45,12 @@ with output_ids:
     
     id_list_code = [f"'{id_}'" for id_ in id_list]
     
-    code = f'''select distinct(ISRC), null as barcode, title, null as mix_name, artist, null as RD, label, label, label, distributor,'0' as price, identifier
+    id_code = f'''select distinct(ISRC), null as barcode, title, null as mix_name, artist, null as RD, label, label, label, distributor,'0' as price, identifier
 from excep_dig_apple_master
 where identifier in ({", ".join(id_list_code)})
 order by identifier;'''
         
-    st.code(code, language='sql')
+    st.code(id_code, language='sql')
     
 with compare_ids:
     
@@ -58,6 +59,42 @@ with compare_ids:
     missing_urls = [url for url, id_ in url_id_map.items() if id_ not in output_ids]
     missing_urls_out = '\n'.join(missing_urls)
     
-    st.code(f"""Hope you are well! 
+    st.code(f"""Hi,
+\nHope you are well! 
 \nThese have been added and flagged, with the exception of the below:
-\n{missing_urls_out}""", language = 'csv')
+\n{missing_urls_out}
+\nThanks,""", language = 'csv')
+
+with flagging_isrcs:
+    
+    isrcs = st.text_area('Paste output ISRCs here')
+    isrcs = isrcs.split("\n")
+    isrc_list = list(isrcs)
+    isrc_list_code = [f"'{isrc}'" for isrc in isrc_list]
+    
+    isrc_code = f"""--Spot check a couple to make sure all is fine
+declare
+
+cursor c_1 is
+select id pro_id
+from products
+where id in (select pro_id
+             from tracks
+             where id in (select tra_id
+                          from ISRCs
+                          where ISRC in ({", ".join(isrc_list_code)})))
+and id not in (select pro_id from pro_pel where pel_id = 37)
+;
+begin
+
+for r_1 in c_1 loop
+insert into pro_pel
+values (r_1.pro_id, '37', trunc(sysdate));
+
+ end loop;
+
+commit;
+
+ end;
+"""
+    st.code(isrc_code, language='sql')
